6559aba52c7fe38f85f2e4c84c0625f082d4b33b
Marcin Bieniek <marcingim@gmail.com>
2023-02-22 12:52:16 +0100

    Add categories section

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,16 +1,88 @@
-import styles from './Home.module.scss'
-
-import { useSelector } from 'react-redux';
+import styles from './Home.module.scss';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Home = () => {
 
-  const products = useSelector(state => state.products);
-  console.log('products:', products)
-
   return (
-    <div>
-      Home
-    </div>
+
+    <Container fluid> 
+      <Row className={styles.container}>
+        <Col md={6} >
+          <div className={styles.slider}>
+            slider
+          </div>
+        </Col>
+
+
+        <Col md={6} className={styles.categories}>
+          <Row className={styles.categories__row}>
+            <Col md={6}>
+              <div className={styles.category}>
+                <img 
+                  src={`${process.env.PUBLIC_URL}/images/categories/unipro-category.jpg`} 
+                  alt="UniPro garage door"
+                />
+                <div className={styles.text}>
+                  <p className={styles.paragraph}>Best sale</p>
+                  <h1 className={styles.headerOne}>Home garage doors</h1>
+                  
+                  <button>More</button>
+                </div>
+                <div className={styles.darkLayer}></div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className={styles.category}>
+                <img 
+                  src={`${process.env.PUBLIC_URL}/images/categories/makropro-category.jpg`} 
+                  alt="UniPro garage door"
+                />
+                <div className={styles.text}>
+                  <p className={styles.paragraph}>Best sale</p>
+                  <h1 className={styles.headerOne}>Industry gates</h1>
+                  <button>More</button>
+                </div>
+                <div className={styles.darkLayer}></div>
+              </div>
+            </Col>
+          </Row>
+          <Row className={styles.categories__row}>
+            <Col md={6}>
+              <div className={styles.category}>
+                <img 
+                  src={`${process.env.PUBLIC_URL}/images/categories/fences-category.jpg`} 
+                  alt="UniPro garage door"
+                />
+                <div className={styles.text}>
+                  <p className={styles.paragraph}>Best sale</p>
+                  <h1 className={styles.headerOne}>Modern fences</h1>
+                  <button>More</button>
+                </div>
+                <div className={styles.darkLayer}></div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className={styles.category}>
+                <img 
+                  src={`${process.env.PUBLIC_URL}/images/categories/windows-category.jpg`} 
+                  alt="UniPro garage door"
+                />
+                <div className={styles.text}>
+                  <p className={styles.paragraph}>Best sale</p>
+                  <h1 className={styles.headerOne}>Warm windows</h1>
+                  <button>More</button>
+                </div>
+                <div className={styles.darkLayer}></div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+
+      </Row>
+    </Container>
+ 
   )
 }
 
