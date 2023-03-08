c88e9f2c96b63bc8ea768c8fcd78a561681715ef
Marcin Bieniek <marcingim@gmail.com>
2023-03-08 12:30:04 +0100

    Add mongo db connection & env file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
 
-app.listen(8000, () => {
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {console.log('DB connection succesfull')})
+  .catch((err) => {console.log('DB error is', err)})
+
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000')
-})
+});
