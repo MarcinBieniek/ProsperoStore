19fd0d7b719c6c47667fcdc5d58d61fbbb0c146b
Marcin Bieniek <marcingim@gmail.com>
2023-03-21 13:51:15 +0100

    Add search display page

--- client/src/components/pages/SearchResult/SearchResult.js
+++ client/src/components/pages/SearchResult/SearchResult.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import { searchInputValue } from '../../../redux/searchRedux';
 import { allProducts } from '../../../redux/productsRedux';
 import ProductSearch from '../../views/ProductSearch/ProductsSearch';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import SearchOffIcon from '@mui/icons-material/SearchOff';
 
 const SearchResult = () => {
 
@@ -17,13 +19,22 @@ const SearchResult = () => {
     <div className={styles.container}>
 
       { search === ''
-        ? <div>Search page is empty.</div>
+        ? <div className={styles.message}>
+            <span>Search page is empty.</span>
+            <span>Type product name.</span>
+            <ManageSearchIcon className={styles.icon}/>
+          </div>
         
         :
 
         <div>
           { filteredSearch.length === 0
-            ? <div>Item not found. Please try again.</div>
+            ? 
+              <div className={styles.message}>
+                <span>Item not found.</span>
+                <span>Please try again.</span>
+                <SearchOffIcon className={styles.icon}/>
+              </div>
             :
             <div>
               <ProductSearch props={filteredSearch} name={"Search results"}/>
